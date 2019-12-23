b221cb93a4196076eaae72ddd554bc4c8d34abea
thisoldbear <danielwilliams.design@gmail.com>
2019-12-23 13:57:12 +0000

    Remove markdown link

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,6 @@ const App: React.FC = () => {
 
   const markdownColumns = `Issue | Description | Completed | Who |\n-- | -- | -- | --`
 
-  const repoUrl = `https://github.com/travellocal`
-
   return (
     <div className="App">
       <header className="App-header">
@@ -44,10 +42,8 @@ const App: React.FC = () => {
                     csv().fromString(csvString)
                       .then((result: Array<CsvResult>) => {
 
-                        console.log(result)
-
                         const issues = result.map((res: CsvResult) =>
-                          `[#${res['Issue Number']}](${repoUrl}/${res['Repo Name']}/pull/${res['Issue Number']}) | ${res['Issue Title']} |  |  `)
+                          `#${res['Issue Number']} | ${res['Issue Title']} |  |  `)
 
                         if (textAreaEl.current) {
                           textAreaEl.current.value = `${markdownColumns}\n${issues.join("\n")}`
